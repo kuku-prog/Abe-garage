6e3c0b2a29560b2ff0d685ca91dcf7df65dfa778
Edil-Mg <135086322+Edil-Mg@users.noreply.github.com>
2023-11-01 22:14:54 -0400

    404 paragraph added

--- codebase/client/src/markup/pages/404.jsx
+++ codebase/client/src/markup/pages/404.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Fo4 = () => {
-  return (
-    <div>404</div>
-  )
-}
+	return (
+		<>
+			<h1>404 - Page Not Found</h1>
+			<p>The page you are looking for does not exist.</p>
+		</>
+	);
+};
 
-export default Fo4
+export default Fo4;
