b7a25df1a486b0d2839cb109e2a9daa7f0a4e88e
zeru0429 <mihiretutigistu@gmail.com>
2023-11-14 07:12:37 +0300

    add customer functionality

---
+++ codebase/server/query/customer.query.js
@@ -0,0 +1,9 @@
+export default {
+   getCustomerByEmailQuery: `SELECT * FROM customer_identifier WHERE customer_email = ? ; `,
+   addCustomer_identifierTableQuery: `INSERT INTO customer_identifier (customer_email, customer_phone_number,customer_added_date, customer_hash)
+   VALUES (?, ?,now(), ?);
+   `,
+   addcustomer_infoTableQuery: `INSERT INTO customer_info (customer_id, customer_first_name, customer_last_name, active_customer_status)
+   VALUES (?, ?, ?, ?);
+   `
+}
---
+++ codebase/server/services/customer.service.js
@@ -0,0 +1,53 @@
+import conn from "../config/db.js";
+import customerQuery from '../query/customer.query.js'
+
+const customerService = {
+
+   getCustomerByEmail: (customerEmail, callback)=>{
+      conn.query(customerQuery.getCustomerByEmailQuery, [customerEmail], (error, result, fields) => {
+         if (error) {
+             console.log(error);
+             return callback(error);
+         } else {
+        
+             return callback(null, result);
+         }
+     });
+   },
+   addTocustomer_identifier: (data, callback)=>{
+    conn.query(customerQuery.addCustomer_identifierTableQuery,
+         [
+            data.customer_email, data.customer_phone, data.customer_hash
+    ],
+     (error, result, fields) => {
+       if (error) {
+           console.log(error);
+           return callback(error);
+       } else {
+      
+           return callback(null, result);
+       }
+   });
+    },
+
+    addTocustomer_ifo: (data, callback)=>{
+        conn.query(customerQuery.addcustomer_infoTableQuery,
+             [
+                data.customer_id, data.customer_first_name, data.customer_last_name, data.active_customer_status
+             ],
+             
+             (error, result, fields) => {
+           if (error) {
+               console.log(error);
+               return callback(error);
+           } else {
+          
+               return callback(null, result);
+           }
+       });
+     },
+
+}
+
+
+export default  customerService;
