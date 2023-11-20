0a3ea75150b71707852e645ee36977aa24cb46db
Edil-Mg <135086322+Edil-Mg@users.noreply.github.com>
2023-11-20 13:51:22 -0500

    image added

--- codebase/client/src/markup/pages/About.jsx
+++ codebase/client/src/markup/pages/About.jsx
@@ -4,7 +4,7 @@ import image8 from '../../assets/images/resource/image8.jpg'
 import vbran1 from "../../assets/images/custom/misc/vban1.jpg";
 import vbran2 from "../../assets/images/custom/misc/vban2.jpg";
 import image4 from '../../assets/images/resource/image4.jpg'
-import backgroundImage from "../../assets/images/background/bg-2.jpg";
+import backgroundImage from "../../assets/images/background/bg2.png";
 import TopBanner from '../components/TopBanner/TopBanner';
 
 const About = () => {
